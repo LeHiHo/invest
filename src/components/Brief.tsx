bb5e2cdca678ec2e9a525be6c159f2a9af4fdff2
Hojin <wkdghwls0623@gmail.com>
2024-03-12 23:56:36 +0900

    Feat: fastapi test

--- src/components/Brief.tsx
+++ src/components/Brief.tsx
@@ -1,20 +1,30 @@
-import { useGetVideo } from "@hooks/useGetVideo";
+// import { useGetVideo } from "@hooks/useGetVideo";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Brief = () => {
-  const videoId = 'ETyDOfnpSzg'; // 예시 videoId, 실제 videoId로 변경 필요
-  const { videoInfo } = useGetVideo(videoId);
+  // const videoId = 'ETyDOfnpSzg'; // 예시 videoId, 실제 videoId로 변경 필요
+  // const { videoInfo } = useGetVideo(videoId);
 
-  console.log(videoInfo)
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // API 호출
+    axios.get('http://127.0.0.1:8000/hello')
+      .then(response => {
+        // 응답 데이터로 상태 업데이트
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []); // 빈 배열을 전달하여 컴포넌트 마운트 시 단 한 번만 실행되게 함
+
 
   return (
-    <>
-      <div>{videoInfo?.title}</div>
-      <div>{videoInfo?.publishedAt}</div>
-      <img src={videoInfo?.thumbnailUrl}/>     
-
-      <div>{videoInfo?.description}</div>
-
-    </>
+    <div>
+      <p>{message}</p>
+    </div>
   );
 };
 
